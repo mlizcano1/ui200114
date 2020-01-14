812bbc117f900ddce2eb961460c9479fe9e1ca32
Lizcano Mauricio <mlizcano@gmail.com>
2020-01-14 19:37:40 +0000

    initial commit

---
+++ UI200114/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sapcp.tutorial.cf.UI200114.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
